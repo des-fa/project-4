03e1be7a103ae3baa7d9ff3a1fc1f687be4291ce
Destinee Fa <destinee.fa@gmail.com>
2022-12-14 12:58:29 +0800

    finished country general info

--- src/pages/countries/[countryId].jsx
+++ src/pages/countries/[countryId].jsx
@@ -1,23 +1,39 @@
 import withAuth from '@/hoc/withAuth'
-// import { useRouter } from 'next/router'
 import React from 'react'
 import { Accordion, Tab, Tabs } from 'react-bootstrap'
 
-function Country({ data, id }) {
-// calling code, emergency phone number
+import countriesData from '../../data/countries.json'
 
+function Country({ countryInfo, id }) {
   // const router = useRouter()
   // const { countryId } = router.query
-  console.log('data', data)
+  console.log('data', countryInfo)
+
+  const currencies = Object.keys(countryInfo?.currencies).map((k, i) => (
+    // Object.entries(data?.currencies[key]).map(([k, v], i) => (
+    // <li key={i}>
+    //   <small>{v}</small>
+    // </li>
+    <li key={i}>
+      <small>{countryInfo?.currencies[k].name}, {countryInfo?.currencies[k].symbol}</small>
+    </li>
+    // ))
+  ))
+
+  const languages = Object.values(countryInfo?.languages).map(
+    (value, i) => <li key={i}><small>{value}</small></li>
+  )
+
+  const timezones = countryInfo?.timezones.map(
+    (timezone, i) => <li key={i}><small>{timezone}</small></li>
+  )
+
+  const countryResult = countriesData.find((country) => country.iso2.toLowerCase() === id.toLowerCase())
 
   // const currencyKey = Object.keys(data?.currencies)
-  // console.log([currencyKey])
   // console.log(data?.currencies[currencyKey].name)
   // console.log(data?.currencies[currencyKey].symbol)
   // console.log('entries', Object.entries(data?.currencies[currencyKey]))
-
-  // console.log('id', id)
-  // console.log(Object.keys(data?.currencies))
 
   // useEffect(async () => {
   //   try {
@@ -32,8 +48,8 @@ function Country({ data, id }) {
   // }, [])
 
   return (
-    <div className="row mx-4 my-4">
-      <div className="col-3 ms-4">
+    <div className="row mx-4 my-5">
+      <div className="col-md-3 ms-4">
         <div>
           <iframe
             src={`https://www.travel-advisory.info/widget-no-js?countrycode=${id}`}
@@ -52,43 +68,43 @@ function Country({ data, id }) {
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Common Name</div>
-                  <small>{data?.name?.common}</small>
+                  <small>{countryInfo?.name?.common}</small>
                 </div>
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Official Name</div>
-                  <small>{data?.name?.official}</small>
+                  <small>{countryInfo?.name?.official}</small>
                 </div>
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Continent</div>
-                  <small>{data?.continents}</small>
+                  <small>{countryInfo?.continents}</small>
                 </div>
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Region</div>
-                  <small>{data?.region}</small>
+                  <small>{countryInfo?.region}</small>
                 </div>
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Subregion</div>
-                  <small>{data?.subregion}</small>
+                  <small>{countryInfo?.subregion}</small>
                 </div>
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Population</div>
-                  <small>{data?.population}</small>
+                  <small>{countryInfo?.population}</small>
                 </div>
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Capital</div>
-                  <small>{data?.capital}</small>
+                  <small>{countryInfo?.capital}</small>
                 </div>
               </li>
             </ul>
@@ -97,17 +113,11 @@ function Country({ data, id }) {
           <Tab eventKey="more" title="More">
             <ul className="list-group list-group-flush">
               <li className="list-group-item d-flex justify-content-between align-items-start">
-                {Object.keys(data?.currencies).length <= 5 ? (
+                {Object.keys(countryInfo?.currencies).length <= 5 ? (
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Currencies</div>
                     <ul>
-                      {Object.keys(data?.currencies).forEach((key) => {
-                        Object.entries(data?.currencies[key]).map(([k, v], i) => (
-                          <li key={i}>
-                            <small>{k}: {v}</small>
-                          </li>
-                        ))
-                      })}
+                      {currencies}
                     </ul>
                   </div>
                 ) : (
@@ -117,13 +127,7 @@ function Country({ data, id }) {
                         <Accordion.Header><span className="fw-bold">Currencies</span></Accordion.Header>
                         <Accordion.Body className="ps-4">
                           <ul>
-                            {Object.keys(data?.currencies).forEach((key) => {
-                              Object.entries(data?.currencies[key]).map(([k, v], i) => (
-                                <li key={i}>
-                                  <small>{k}: {v}</small>
-                                </li>
-                              ))
-                            })}
+                            {currencies}
                           </ul>
                         </Accordion.Body>
                       </Accordion.Item>
@@ -132,13 +136,11 @@ function Country({ data, id }) {
                 )}
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
-                {Object.keys(data?.languages).length <= 5 ? (
+                {Object.keys(countryInfo?.languages).length <= 5 ? (
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Languages</div>
                     <ul>
-                      {Object.values(data?.languages).map(
-                        (value, i) => <li key={i}><small>{value}</small></li>
-                      )}
+                      {languages}
                     </ul>
                   </div>
                 ) : (
@@ -148,9 +150,7 @@ function Country({ data, id }) {
                         <Accordion.Header><span className="fw-bold">Languages</span></Accordion.Header>
                         <Accordion.Body className="accordion-btn-padding-x-0">
                           <ul className="ps-4">
-                            {Object.values(data?.languages).map(
-                              (value, i) => <li key={i}><small>{value}</small></li>
-                            )}
+                            {languages}
                           </ul>
                         </Accordion.Body>
                       </Accordion.Item>
@@ -159,13 +159,11 @@ function Country({ data, id }) {
                 )}
               </li>
               <li className="list-group-item d-flex justify-content-between align-items-start">
-                {Object.keys(data?.timezones).length <= 5 ? (
+                {Object.keys(countryInfo?.timezones).length <= 5 ? (
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">Timezones</div>
                     <ul>
-                      {data?.timezones.map(
-                        (timezone, i) => <li key={i}><small>{timezone}</small></li>
-                      )}
+                      {timezones}
                     </ul>
                   </div>
                 ) : (
@@ -175,9 +173,7 @@ function Country({ data, id }) {
                         <Accordion.Header><span className="fw-bold">Timezones</span></Accordion.Header>
                         <Accordion.Body className="accordion-btn-padding-x-0">
                           <ul className="ps-4">
-                            {data?.timezones.map(
-                              (timezone, i) => <li key={i}><small>{timezone}</small></li>
-                            )}
+                            {timezones}
                           </ul>
                         </Accordion.Body>
                       </Accordion.Item>
@@ -188,10 +184,16 @@ function Country({ data, id }) {
               <li className="list-group-item d-flex justify-content-between align-items-start">
                 <div className="ms-2 me-auto">
                   <div className="fw-bold">Driving Side</div>
-                  <small className="text-capitalize">{data?.car?.side}</small>
+                  <small className="text-capitalize">{countryInfo?.car?.side}</small>
                 </div>
               </li>
-              <a href={data?.maps?.googleMaps}>Google</a>
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Calling Code</div>
+                  <small className="text-capitalize">+{countryResult?.phone_code}</small>
+                </div>
+              </li>
+              <a href={countryInfo?.maps?.googleMaps}>Google</a>
             </ul>
           </Tab>
         </Tabs>
@@ -201,13 +203,16 @@ function Country({ data, id }) {
 }
 
 export async function getServerSideProps(context) {
-  // console.log(context.params.countryId)
   // Fetch data from external API
-  const res = await fetch(`https://restcountries.com/v3.1/alpha/${context.params.countryId}`)
-  const data = await res.json()
+  const [countryInfoRes] = await Promise.all([
+    fetch(`https://restcountries.com/v3.1/alpha/${context.params.countryId}`)
+  ])
+  const [countryInfo] = await Promise.all([
+    countryInfoRes.json()
+  ])
   // Pass data to the page via props
   return { props: {
-    data: data[0],
+    countryInfo: countryInfo[0],
     id: context.params.countryId
   } }
 }
