99a8bd543139cdc862468b999e7ff86028565c64
Destinee Fa <destinee.fa@gmail.com>
2022-12-19 14:41:27 +0800

    added message and follow buttons logic

--- src/pages/users/[userId].jsx
+++ src/pages/users/[userId].jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Router from 'next/router'
 import { getSession } from 'next-auth/react'
 
@@ -10,46 +10,156 @@ import useUserProfile from '@/hooks/users/profile'
 import useUserVisitedCountries from '@/hooks/users/visited-countries'
 import useUserPlans from '@/hooks/users/plans'
 
-function UserProfile({ profile }) {
+function UserProfile({ profile, currentUser }) {
   const { userVisitedCountries } = useUserVisitedCountries()
   const { userPlans } = useUserPlans()
+  // console.log(profile?.user)
+  // console.log(currentUser)
+
+  const followIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-plus-fill" viewBox="0 0 16 16">
+      <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+      <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+    </svg>
+  )
+
+  const followingIcon = (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-check-fill" viewBox="0 0 16 16">
+      <path fillRule="evenodd" d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+      <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+    </svg>
+  )
+
+  const [followButton, setFollowButton] = useState('true')
+  const [followText, setFollowText] = useState('Follow')
+  const [followImg, setFollowImg] = useState(followIcon)
+  const [showMessageButton, setShowMessageButton] = useState(false)
+  const [showFollowsYouText, setShowFollowsYouText] = useState(false)
+
+  const followsCurrentUser = profile?.user?.following?.filter((follower) => follower.followingId === currentUser)
+
+  const followingUser = profile?.user?.followedBy?.filter((follower) => follower.followerId === currentUser)
+
+  // console.log('follows me', followsCurrentUser)
+  // console.log('follows profile', followingUser)
+
+  // changing buttons and follow message dynamically
+  useEffect(() => {
+    if (followsCurrentUser?.length !== 0 && followingUser?.length !== 0) {
+      setFollowButton(false)
+      setFollowText('Following')
+      setFollowImg(followingIcon)
+      setShowMessageButton(true)
+      setShowFollowsYouText(true)
+    } else if (followingUser?.length !== 0 && followsCurrentUser?.length === 0) {
+      setFollowButton(false)
+      setFollowText('Following')
+      setFollowImg(followingIcon)
+      setShowMessageButton(true)
+      setShowFollowsYouText(false)
+    } else if (followingUser?.length === 0 && followsCurrentUser?.length > 0) {
+      setFollowButton(true)
+      setFollowText('Follow Back')
+      setFollowImg(followIcon)
+      setShowMessageButton(true)
+      setShowFollowsYouText(true)
+    } else {
+      setFollowButton(true)
+      setFollowText('Follow')
+      setFollowImg(followIcon)
+      setShowFollowsYouText(false)
+      setShowMessageButton(false)
+    }
+  }, [followingUser, followsCurrentUser])
+
+  // clicking on follow button updates followers/following
+  // const handleClick = followingUser?.length !== 0 ? (
+  //   (value) => {
+  //     deleteMyFollowing(value).unwrap().then(() => {
+  //       // console.log('unfollowed')
+  //     })
+  //   }
+  // ) : (
+  //   (value) => {
+  //     createMyFollowing(value).unwrap().then(() => {
+  //       // console.log('followed')
+  //     })
+  //   }
+  // )
 
   return (
-    <div className="d-flex flex-lg-row flex-md-row flex-column justify-content-evenly gap-4">
-      <div className="card col-lg-3 col-md-3">
-        <Image
-          className="card-profile-picture img-fluid"
-          src={profile?.avatar}
-          alt="profile-picture"
-          style={{ maxHeight: '250px' }}
-        />
-        <div className="card-body my-2">
-          <div className="d-flex flex-row justify-content-between mb-2">
-            <h4 className="fw-semibold text-capitalize">{profile?.fullName}</h4>
-          </div>
+    <>
+      <div className="d-flex flex-row justify-content-end me-4 py-2 gap-3">
+        <button
+          type="button"
+          className="d-flex flex-row gap-2 align-items-center btn btn-sm btn-dark text-white px-3"
+          style={{ visibility: showMessageButton ? 'visible' : 'hidden' }}
+        >
+          Message
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
+            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+          </svg>
+        </button>
 
-          <h6>{profile?.about}</h6>
+        <button
+          type="button"
+          // className="btn btn-sm btn-dark text-white px-3"
+          className={followButton ? 'd-flex flex-row gap-2 align-items-center btn btn-sm btn-dark text-white px-3' : 'd-flex flex-row gap-2 align-items-center btn btn-sm btn-outline-secondary px-3'}
+        >
+          {/* <div className="me-2"> */}
+          {followText}
+          {/* </div> */}
+          {followImg}
+        </button>
+      </div>
+
+      <div className="d-flex flex-row justify-content-end me-4 mb-2">
+        <small
+          className="text-muted text-decoration-underline"
+          style={{ visibility: showFollowsYouText ? 'visible' : 'hidden' }}
+        >
+          Follows you
+        </small>
+      </div>
+
+      <div className="d-flex flex-lg-row flex-md-row flex-column justify-content-evenly gap-4">
+        <div className="card col-lg-3 col-md-3">
+          <Image
+            className="card-profile-picture img-fluid"
+            src={profile?.avatar}
+            alt="profile-picture"
+            style={{ maxHeight: '250px' }}
+          />
+          <div className="card-body my-2">
+            <div className="d-flex flex-row justify-content-between mb-2">
+              <h4 className="fw-semibold text-capitalize">{profile?.fullName}</h4>
+            </div>
+
+            <h6>{profile?.about}</h6>
+          </div>
+        </div>
+
+        <div className="col-lg-8 col-md-8 border border-gray rounded p-4">
+          <UserProfileTabs
+            userVisitedCountries={userVisitedCountries}
+            userPlans={userPlans}
+          />
         </div>
       </div>
-
-      <div className="col-lg-8 col-md-8 border border-gray rounded p-4">
-        <UserProfileTabs
-          userVisitedCountries={userVisitedCountries}
-          userPlans={userPlans}
-        />
-      </div>
-    </div>
-
+    </>
   )
 }
 
 export function UserPage() {
+  const { userProfile, isLoadingUserProfile, isError, errorMessage } = useUserProfile()
+  const [currentUser, setCurrentUser] = useState('')
+
   const checkUser = async () => {
     const session = await getSession()
     const { pathname } = Router
-    const currentUser = session?.user?.id
-    console.log(session?.user?.id)
-    console.log(pathname)
+    setCurrentUser(session?.user?.id)
+    // console.log(currentUser)
+    // console.log(pathname)
 
     if (pathname === `/users/${currentUser}`) {
       console.log('not right')
@@ -59,10 +169,7 @@ export function UserPage() {
 
   useEffect(() => {
     checkUser()
-  }, [])
-
-  const { userProfile, isLoadingUserProfile, isError, errorMessage } = useUserProfile()
-  console.log(userProfile)
+  }, [userProfile])
 
   let content
 
@@ -76,39 +183,17 @@ export function UserPage() {
   if (isError) {
     content = (
       <div>
-        <h3 className="text-muted fw-light m-4">{errorMessage}.</h3>
+        <h3 className="text-muted fw-light m-4">{errorMessage}</h3>
       </div>
     )
   }
 
   if (userProfile) {
     content = (
-      <div className="w-100">
-        <div className="d-flex flex-row justify-content-end mb-3 me-4 py-2 gap-3">
-          <button
-            type="button"
-            className="btn btn-sm btn-dark text-white px-3 py-2"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-send-fill" viewBox="0 0 16 16">
-              <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
-            </svg>
-          </button>
-
-          <button
-            type="button"
-            className="btn btn-sm btn-dark text-white px-3 py-2"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-person-plus-fill" viewBox="0 0 16 16">
-              <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-              <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
-            </svg>
-          </button>
-        </div>
-
-        <UserProfile
-          profile={userProfile}
-        />
-      </div>
+      <UserProfile
+        profile={userProfile}
+        currentUser={currentUser}
+      />
     )
   }
 
