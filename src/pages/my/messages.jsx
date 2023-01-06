8b4bb65e75792d1cf8ba486789836cfa766be184
Destinee Fa <destinee.fa@gmail.com>
2023-01-06 11:54:57 +0800

    commented out chatroom

--- src/pages/my/messages.jsx
+++ src/pages/my/messages.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 // import { ref, onValue } from 'firebase/database'
 
 // import { getSession } from 'next-auth/react'
-import { database } from '@/services/firebaseconfig'
+// import { database } from '@/services/firebaseconfig'
 
-import useMyUser from '@/hooks/my/user'
-import ChatRoom from '@/components-layouts/ChatRoom'
+// import useMyUser from '@/hooks/my/user'
+// import ChatRoom from '@/components-layouts/ChatRoom'
 import withAuth from '@/hoc/withAuth'
 
 export function MessagesPage() {
-  const { myUser } = useMyUser()
+  // const { myUser } = useMyUser()
 
   return (
     <div className="container">
@@ -17,7 +17,7 @@ export function MessagesPage() {
         Messages
       </h1>
 
-      <ChatRoom myUser={myUser} db={database} />
+      {/* <ChatRoom myUser={myUser} db={database} /> */}
     </div>
 
   )
